643817132c746852bfe107f4ead2e73ba19986d7
bootstet <chenyouzeng@chenyouzengdeMacBook-Air.local>
2020-07-23 11:09:40 +0800

    lint-staged

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,16 @@
 const path = require('path')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+// const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'none',
   entry: './src/main.js',
   output: {
     filename: '[name]-[hash].bundle.js',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist')
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -31,25 +31,25 @@ module.exports = {
       {
         test: /.js$/,
         use: {
-          loader: 'eslint-loader',
+          loader: 'eslint-loader'
         },
         enforce: 'pre'
       },
       {
         test: /.css$/,
-        use: {
-          loader: 'style-loader',
-          loader: 'css-loader',
-        }
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       },
       {
         test: /\.less$/,
         use: ['style-loader', 'css-loader', 'less-loader', {
-            loader: 'style-resources-loader',
-            options: {
-              patterns: path.resolve(__dirname, 'path/to/less/variables/*.less'),
-              // injector: 'append'
-            }
+          loader: 'style-resources-loader',
+          options: {
+            patterns: path.resolve(__dirname, 'path/to/less/variables/*.less')
+            // injector: 'append'
+          }
         }]
       },
       {
@@ -57,29 +57,29 @@ module.exports = {
         use: {
           loader: 'vue-loader'
         }
-      }, 
+      },
       {
         test: /.(png|jpg|ico)$/,
         use: {
           loader: 'url-loader',
           options: {
             esModule: false,
-            limit: 10 
+            limit: 10
           }
         }
-      },
+      }
     ]
   },
   plugins: [
-    // new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'webpack-vue',
       meta: {
-        viewport: 'width=decice-width',
+        viewport: 'width=decice-width'
       },
-      template: './public/index.html',
+      template: './public/index.html'
     }),
-    new VueLoaderPlugin(),
+    new VueLoaderPlugin()
     // new webpack.DefinePlugin({
     //   BASE_URL: '""'
     // })
